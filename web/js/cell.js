87da2ba588c99957250e55883fba4f0020167cdc
eelizanjr <eelizanjr@gmail.com>
2017-04-09 14:44:05 -0400

    finalized

---
+++ web/js/cell.js
@@ -0,0 +1,34 @@
+// the cell object
+function Cell () {
+  // state flag - alive or not
+  this.element = $("<div class='cell " + russianRoulette() + "'></div>");
+  // reference to the number of it's live neighbors
+  // to determine if it's alive or dead the next turn
+  this.neighbors = 0;
+
+  // randomly returns to be alive (30%) of the time
+  // returns to the cell div
+  function russianRoulette() {
+    if(Math.random() > 0.7) {
+      return "alive";
+    } else {
+      return "";
+    }
+  }
+
+  // when we bring a cell to life, we add he .alive class
+  Cell.prototype.resurrect = function() {
+    this.element.addClass('alive');
+  }
+
+  // when we kill a cell we remove .alive class
+  Cell.prototype.kill = function() {
+    this.element.removeClass('alive');
+  }
+
+  // check if cell is alive
+  Cell.prototype.isAlive = function () {
+    return this.element.hasClass('alive');
+  }
+
+}
---
+++ web/js/index.js
@@ -0,0 +1,54 @@
+var interval;
+
+
+function go(game) {
+  // the frequency of execution
+  // what to do and at rate
+  // updateNeighbors and updateStates every 1 second
+  interval = setInterval(function(){
+    game.updateNeighbors();
+    game.updateStates();
+    console.log("Interval count");
+  }, 100)
+}
+
+// run on document ready because we want the grid to exist
+$(document).ready(function() {
+  var size = 10;
+ // create grid
+  var conway = new Conway(size);
+  // put it on the page
+  conway.renderGrid();
+
+  // get row widthe to center content
+  // used by the .cell class in style.css
+  var rowWidth = $('.cell').width() * 40;
+  $('.row').width(rowWidth);
+
+  /*
+    USER EVENTS
+  */
+
+  $('.cell').on('click', function(){
+    // this refers to the cell that was clicked
+    // note, use jquery syntax to make 'this' a jquery object
+    console.log(this);
+    $(this).toggleClass('alive');
+
+  });
+
+// start game when user press enter
+$(document).on('keypress', function(e){
+  // when user presses enter
+  if(e.keyCode === 13) {
+      go(conway);
+    }
+    // when user hits spacebar - clear the grid
+    if(e.keyCode === 32) {
+      $('.alive').removeClass('alive');
+      // stop the time interval which will pause the game
+      clearInterval(interval);
+    }
+  });
+
+});
